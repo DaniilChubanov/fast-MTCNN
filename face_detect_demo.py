fix bbox resizer calculation

face_detect_demo.py
@@ -11,6 +11,8 @@ from align import detect_face
 import tensorflow as tf
 import sys
 import resize
+from scipy import misc
+
 sys.path.append('..')
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -26,10 +28,10 @@ os.environ["NUMEXPR_NUM_THREADS"] = "20"  # export NUMEXPR_NUM_THREADS=6
 
 # some constants kept as default from facenet
 minsize = 30
-threshold = [0.65, 0.65, 0.70]
-factor = 0.7098
+threshold = [0.60, 0.65, 0.65]
+factor = 0.709
 margin = 0
-
+face_crop_size=160
 sess = tf.Session()
 # read pnet, rnet, onet models from align directory and files are det1.npy, det2.npy, det3.npy
 pnet, rnet, onet = detect_face.create_mtcnn(
@@ -38,18 +40,18 @@ pnet, rnet, onet = detect_face.create_mtcnn(
 
 def getFace(img, compress_frame=True):
     faces = []
-    bounding_boxes=None
+    bounding_boxes = None
     #print (type(img))
     img_size = img.shape[0:2]
     if compress_frame is True:
-        resize_img = resize.frame_resizer(img, 400)
+        resize_img = resize.frame_resizer(img, 300)
         bounding_boxes, _ = detect_face.detect_face(
-        resize_img, minsize, pnet, rnet, onet, threshold, factor)
+            resize_img, minsize, pnet, rnet, onet, threshold, factor)
         bounding_boxes = bbox_resizer(
             img, bounding_boxes, size_frame=resize_img.shape[0:2])
         #img = np.resize(img, (288, 352))
     #print (img.shape)
-    #print(img_size)
+    # print(img_size)
     else:
         bounding_boxes, _ = detect_face.detect_face(
             img, minsize, pnet, rnet, onet, threshold, factor)
@@ -59,7 +61,7 @@ def getFace(img, compress_frame=True):
             if face[4] > 0.50:
                 #det = np.squeeze(face[0:4])
                 bb = np.zeros(4, dtype=np.int32)
-                _margin = float(np.divide(margin, 2))
+                _margin = int(np.divide(margin, 2))
                 bb[0] = np.maximum(np.subtract(
                     face[0], _margin), -float(img_size[1]))
                 bb[1] = np.maximum(np.subtract(
@@ -69,16 +71,21 @@ def getFace(img, compress_frame=True):
                 bb[3] = np.minimum(np.add(face[3], _margin),
                                    float(img_size[0]))
                 cropped = img[bb[1]: bb[3], bb[0]: bb[2], :]
+                #_cropped = misc.imresize(
+                #    cropped, (face_crop_size, face_crop_size), interp='bilinear')
+                #_cropped=cv2.resize(cropped,(face_crop_size,face_crop_size),interpolation=cv2.INTER_LINEAR)
                 #print (cropped.shape)
                 #resized = cv2.resize(cropped, dsize=(input_image_size, input_image_size))
-                #cv2.imshow("resize", cropped)
+                #cv2.imshow("resize", _cropped)
+                #cv2.imshow("crop",cropped)
                 # print(resized.shape)
                 faces.append({'face': cropped, 'rect': [
                              bb[0], bb[1], bb[2], bb[3]], 'acc': face[4]})
                 #print (faces)
     return faces
 
-@numba.autojit
+
+#@numba.autojit
 def bbox_resizer(frame, bboxs, size_frame):
     # imageToPredict = cv2.imread("img.jpg", 3)
     # Note: flipped comparing to your original code!
@@ -88,22 +95,16 @@ def bbox_resizer(frame, bboxs, size_frame):
     for bbox in bboxs:
         y_ = frame.shape[0]
         x_ = frame.shape[1]
-        x_scale = np.divide(size_frame[0], x_)
-        y_scale = np.divide(size_frame[1], y_)
-        #print(x_scale, y_scale)
-        #frame = cv2.resize(frame, resize);
-        # print(img.shape)
-        #img = np.array(img);
-
-        # original frame as named values
-        # print(bbox[0:4])
+       
+        x_scale = np.divide(x_,size_frame[1])
+        y_scale = np.divide(y_,size_frame[0])
         origLeft, origTop, origRight, origBottom = bbox[0:4]
-        #print ("x_scale",x_scale)
         x = int(np.round(np.multiply(origLeft, x_scale)))
         y = int(np.round(np.multiply(origTop, y_scale)))
         xmax = int(np.round(np.multiply(origRight, x_scale)))
         ymax = int(np.round(np.multiply(origBottom, y_scale)))
         new_bbox.append([x, y, xmax, ymax, bbox[4]])
+        
     return np.asarray(new_bbox)
 
 
@@ -153,20 +154,15 @@ if __name__ == "__main__":
     vs = WebcamVideoStream(src=0, width=640, height=480, fps=60).start()
     while True:
         frame = vs.read()
-        # if ret:
-        #frame = np.array(frame)
-    #img = imutils.resize(img, width=1000)
         timestart = time.clock()
-        print("size",vs.stream.get(cv2.CAP_PROP_FRAME_HEIGHT),vs.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
-        faces = getFace(frame,compress_frame=False)
+        faces = getFace(frame, compress_frame=False)
         print(time.clock()-timestart)
         for face in faces:
             cv2.rectangle(frame, (face['rect'][0], face['rect'][1]),
-                          (face['rect'][2], face['rect'][3]), (0, 255, 0), 2)
+                          (face['rect'][2], face['rect'][3]), (255, 255, 0), 2)
         cv2.imshow("faces", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
     vs.stop()
-
+    vs.__exit__()
     cv2.destroyAllWindows()

resize.py
@@ -8,17 +8,19 @@ stream.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
 stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
 stream.set(cv2.CAP_PROP_FPS,60)"""
 
+
 @autojit
 def frame_resizer(frame, height):
-    weight_size = np.divide(height , frame.shape[0])
-    dim = (int(np.multiply( frame.shape[1] , weight_size)), height)
-    resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+    weight_size = np.divide(height, frame.shape[0])
+    dim = (int(np.multiply(frame.shape[1], weight_size)), height)
+    resized = cv2.resize(frame, dim, interpolation=cv2.INTER_CUBIC)
     return resized
+
 
 """while True:
     ret,image = stream.read()
     cv2.imshow("Original", image)
-    #print("orgin", image.shape)
+    # print("orgin", image.shape)
     resized = frame_resizer(image, 300)
     print(resized.shape)
     cv2.imshow("Resized (Height) ", resized)
@@ -27,19 +29,22 @@ def frame_resizer(frame, height):
 stream.release()
 cv2.waitKey(0)"""
 
-def crop(self, image, bbox):
-        """
-        Crop an image according to a bounding box
-        :param bbox: the bounding box
-        :return: the cropped image
-        """
-        _image = copy.deepcopy(image)
-        bounding_box = np.zeros(4, dtype=np.int32)
-        img_size = np.asarray(_image.shape)[0:2]
-        bounding_box[0] = np.maximum(bbox[0] - self.face_crop_margin / 2, 0)
-        bounding_box[1] = np.maximum(bbox[1] - self.face_crop_margin / 2, 0)
-        bounding_box[2] = np.minimum(bbox[2] + self.face_crop_margin / 2, img_size[1])
-        bounding_box[3] = np.minimum(bbox[3] + self.face_crop_margin / 2, img_size[0])
-        cropped = _image[bounding_box[1]:bounding_box[3], bounding_box[0]:bounding_box[2], :]
-        cropped = misc.imresize(cropped, (self.face_crop_size, self.face_crop_size), interp='bilinear')
-        return cropped
+
+def crop(self, image, bbox, face_crop_size, face_crop_margin):
+    """
+    Crop an image according to a bounding box
+    :param bbox: the bounding box
+    :return: the cropped image
+    """
+    _image = copy.deepcopy(image)
+    bounding_box = np.zeros(4, dtype=np.int32)
+    img_size = np.asarray(_image.shape)[0:2]
+    bounding_box[0] = np.maximum(bbox[0] - face_crop_margin / 2, 0)
+    bounding_box[1] = np.maximum(bbox[1] - face_crop_margin / 2, 0)
+    bounding_box[2] = np.minimum(bbox[2] + face_crop_margin / 2, img_size[1])
+    bounding_box[3] = np.minimum(bbox[3] + face_crop_margin / 2, img_size[0])
+    cropped = _image[bounding_box[1]:bounding_box[3],
+                     bounding_box[0]:bounding_box[2], :]
+    cropped = misc.imresize(
+        cropped, (face_crop_size, face_crop_size), interp='bilinear')
+    return cropped